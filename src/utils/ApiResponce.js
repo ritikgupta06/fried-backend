d756b080ffca88d7ad721c8a5ae0b7136f90e51b
ritikgupta06 <onlyritik10@gmail.com>
2025-03-15 17:10:25 +0530

    .

--- src/utils/ApiResponce.js
+++ src/utils/ApiResponce.js
@@ -1,7 +1,7 @@
 class ApiResponce {
   constructor(data, message) {
     this.statusCode = statusCode;
-    this.success = this.statusCode;
+    this.success = statusCode < 400;
     this.data = data;
     this.message = message;
   }
